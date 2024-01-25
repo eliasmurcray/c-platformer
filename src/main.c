b39d57e78c4e59cfb7240cf32aa901fb5cfba395
eliasmurcray <eliasmurcray@gmail.com>
2024-01-25 13:07:42 -0800

    Rendering an image

--- src/main.c
+++ src/main.c
@@ -6,31 +6,58 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main(int argc, char *args[]) {
-	SDL_Window *window = NULL;
-	SDL_Surface *surface = NULL;
+SDL_Window *gWindow = NULL;
+SDL_Surface *gSurface = NULL;
+SDL_Surface *gBmapSurface = NULL;
 
+bool init() {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		printf("SDL could not initialize! SDL_Error: %s\n",
-		       SDL_GetError());
+		return false;
+	}
+
+	gWindow = SDL_CreateWindow("My First Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+	if (gWindow == NULL) {
+		return false;
+	}
+
+	gSurface = SDL_GetWindowSurface(gWindow);
+
+	return true;
+}
+
+bool load_assets() {
+	gBmapSurface = SDL_LoadBMP("assets/test-asset.bmp");
+	return gBmapSurface != NULL;
+}
+
+void exit_SDL() {
+	SDL_FreeSurface(gBmapSurface);
+	gBmapSurface = NULL;
+
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	SDL_Quit();
+}
+
+int main(int argc, char *args[]) {
+	if (!init()) {
+		printf("Failed to initialize! SDL_Error: %s\n", SDL_GetError());
 		return EXIT_FAILURE;
 	}
 
-	window = SDL_CreateWindow("My First Game", SDL_WINDOWPOS_UNDEFINED,
-				  SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-				  SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-	if (window == NULL) {
-		printf("Failed to create SDL window! SDL_Error: %s\n",
-		       SDL_GetError());
-		return EXIT_FAILURE;
+	if (!load_assets()) {
+		printf("Failed to load assets! SDL_Error: %s\n", SDL_GetError());
+		return EXIT_SUCCESS;
 	}
 
-	surface = SDL_GetWindowSurface(window);
+	SDL_BlitSurface(gBmapSurface, NULL, gSurface, NULL);
 
-	SDL_FillRect(surface, NULL,
-		     SDL_MapRGB(surface->format, 0xff, 0xff, 0xff));
+	//SDL_FillRect(gSurface, NULL,
+		     //SDL_MapRGB(gSurface->format, 0xff, 0xff, 0xff));
 
-	SDL_UpdateWindowSurface(window);
+	SDL_UpdateWindowSurface(gWindow);
 
 	SDL_Event e;
 	bool quit = false;
@@ -41,9 +68,7 @@ int main(int argc, char *args[]) {
 		}
 	}
 
-	SDL_DestroyWindow(window);
-
-	SDL_Quit();
+	exit_SDL();
 
 	return EXIT_SUCCESS;
 }
